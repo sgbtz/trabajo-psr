059a53d5d38352516c53ea8907ef238e0342851d
salas <salas@trajano.us.es>
2019-01-19 16:40:29 +0100

    empezando escenario

---
+++ Simulacion.h
@@ -0,0 +1,47 @@
+#ifndef SIMULACION_H
+#define SIMULACION_H
+
+// includes de la aplicación
+#include "ns3/header.h"
+#include "ns3/ppp-header.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/application.h"
+#include "ns3/stats-module.h"
+#include "ns3/core-module.h"
+#include "ns3/object.h"
+#include "ns3/global-value.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/point-to-point-net-device.h"
+#include "ns3/point-to-point-channel.h"
+#include "ns3/point-to-point-remote-channel.h"
+#include "ns3/point-to-point-helper.h"
+#include "ns3/random-variable-stream.h"
+#include "ns3/attribute.h"
+#include "ns3/onoff-application.h"
+#include "ns3/tag.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/stats-module.h"
+#include "ns3/log.h"
+#include "ns3/abort.h"
+#include "ns3/queue-limits.h"
+#include "ns3/queue.h"
+#include "ns3/net-device-queue-interface.h"
+#include "ns3/uinteger.h"
+#include "ns3/pointer.h"
+#include "ns3/traffic-control-layer.h"
+#include "ns3/traffic-control-helper.h"
+#include "ns3/inet-socket-address.h"
+#include <ns3/gnuplot.h>
+#include <ns3/queue.h>
+#include <ostream>
+#include <map>
+
+using namespace ns3;
+
+// observador
+
+#endif
--- simulacion.cc
+++ simulacion.cc
@@ -20,39 +20,57 @@
 #include "ns3/random-variable-stream.h"
 #include "ns3/traffic-control-helper.h"
 #include "ns3/gnuplot.h"
-#include "Observador.h"
+#include "Simulacion.h" // fichero de cabecera 
 
 using namespace ns3;
+NS_LOG_COMPONENT_DEFINE ("simulacion");
+
+/* 
+** EN ESTE CÓDIGO SE VAN A CREAR LOS ENLACES P2P NECESARIOS PARA CONSTRUIR ESTE ESCENARIO
+**
+**
+**                        servidor    servidor    servidor
+**                                \      |       / 
+**  grupo enlaces 1 -->            \     |      /
+**                                  \    |     /
+**                                    routerSer
+**                                       /  \
+**  grupo enlaces 2 -->                 /    \
+**                                     /      \
+**                               routerCam     routerUsr
+**                               /  |  \         /  |  \
+**  grupo enlaces 3 -->         /   |   \       /   |   \        <-- grupo enlaces 4
+**                             /    |    \     /    |    \
+**                           cam   cam   cam  usr  usr   usr
+**
+**
+** UNA VEZ TENGAMOS LOS NODOS SE CREAN LAS APLICACIONES ONOFF UDP/TCP SOBRE ELLOS
+**
+*/
 
 /************* Definición de macros **************/
-#define ROUTER_RATE  "10Mb/s"
-#define SERVER_RATE  "5Mb/s"
-#define CAM_RATE     "3Mb/s"
-#define USER_RATE    "1Mb/s"
-#define ROUTER_DELAY "10ms"
-#define SERVER_DELAY "1ms"
-#define CAM_DELAY    "2ms"
-#define USER_DELAY   "3ms"
-#define PERROR 0.01
-#define T_ON "1s"
-#define T_OFF "0s"
-#define T_START "1s"
-#define T_STOP  "1h"
-#define ON_RATE "500kb/s"
-#define PCKT_LEN 1220
-#define TSTUDENT 2.2622
-#define MUESTRAS 10
-#define UDP 0
-#define TCP 1
+
+// rate de cada grupo
+#define SERVER_RATE  "5Mb/s"	// g1
+#define ROUTER_RATE  "10Mb/s"	// g2
+#define CAM_RATE     "3Mb/s"	// g3
+#define USER_RATE    "1Mb/s"	// g4
+
+// delay de cada grupo
+#define SERVER_DELAY "1ms"		// g1
+#define ROUTER_DELAY "10ms"		// g2
+#define CAM_DELAY    "2ms"		// g3
+#define USER_DELAY   "3ms"		// g4
+
 
 /************** Definición de tipos ***************/
-// Estructura para los enlaces del esecenario
+
+// Estructura para los enlaces del escenario
 typedef struct {
-  DataRate rate; // capacidad del enlace
-  Time delay; // Retardo de propagación del enlace
+  DataRate rate; 	// capacidad del enlace
+  Time delay; 		// Retardo de propagación del enlace
 } LinkP2P;
 
-NS_LOG_COMPONENT_DEFINE ("simulacion");
 
 /************ Definición de funciones *************/
 /*
@@ -63,6 +81,7 @@ NS_LOG_COMPONENT_DEFINE ("simulacion");
 **  - Parámetros de salida:
 **
 */
+
 void escenario(LinkP2P routerLink, LinkP2P serverLink, LinkP2P camLink, LinkP2P userLink);
 
 int main (int argc, char *argv[]) {
@@ -71,25 +90,22 @@ int main (int argc, char *argv[]) {
   GlobalValue::Bind ("ChecksumEnabled", BooleanValue(true));
 
   // Parámetros del modelo
-  // - del enlace punto a punto de los encaminadores
-  LinkP2P routerLink;
-  routerLink.rate(ROUTER_RATE);  // capacidad del enlace entre encaminadores
-  routerLink.delay(ROUTER_DELAY); // retardo de propagación del enlace entre encaminadores
-  // - de los enlaces punto a punto a los servidores
-  LinkP2P serverLink;
-  serverLink.rate(SERVER_RATE);  // capacidad del enlace entre encaminadores
-  serverLink.delay(SERVER_DELAY); // retardo de propagación del enlace entre encaminadores
-  // - de los enlaces punto a punto a las camaras
-  LinkP2P camLink;
-  camLink.rate(CAM_RATE);  // capacidad del enlace entre encaminadores
-  camLink.delay(CAM_DELAY); // retardo de propagación del enlace entre encaminadores
-  // - de los enlaces punto a punto a los usuarios
-  LinkP2P userLink;
-  userLink.rate(USER_RATE);  // capacidad del enlace entre encaminadores
-  userLink.delay(USER_DELAY); // retardo de propagación del enlace entre encaminadores
 
+  // grupo 1
+  DataRate serverRate (SERVER_RATE);
+  Time serverDelay (SERVER_DELAY);
 
+  // grupo 2
+  DataRate routerRate (ROUTER_RATE);
+  Time routerDelay (ROUTER_DELAY);  
 
+  // grupo 3
+  DataRate camRate (CAM_RATE);
+  Time camDelay (CAM_DELAY);
+
+  // grupo 4
+  DataRate usrRate (USER_RATE);
+  Time usrDelay (USER_DELAY);
 
   return 0;
 }
